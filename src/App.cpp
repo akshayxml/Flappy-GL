4df94794f47d17a35a0a8a85810ba4b6a86b01b1
Akshay <akshayknr7@gmail.com>
2021-05-19 10:12:02 +0530

    added bg

--- src/App.cpp
+++ src/App.cpp
@@ -16,13 +16,21 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window, int key, int scancode, int action, int mods);
-void textureSetup(unsigned int& id, const char* fp);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+unsigned int loadTexture(const char* fp);
 
 const unsigned int SCR_WIDTH = 1920;
 const unsigned int SCR_HEIGHT = 1080; 
 float current_opacity = 0.0;
 float deltaTime = 0.0f;	// Time between current frame and last frame
 float lastFrame = 0.0f; // Time of last frame
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 2.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouseMovement = true;
 
 int flyUp = 0;
 
@@ -53,26 +61,35 @@ int main(){
     }
 
     glfwSetKeyCallback(window, processInput);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
 
-    //glEnable(GL_DEPTH_TEST);
+    glEnable(GL_DEPTH_TEST);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     // build and compile our shader program
-    Shader shaderProgram("shaders/vertex.glsl", "shaders/fragment.glsl");
+    Shader birdShader("shaders/bird.vs", "shaders/bird.fs");
+    Shader bgShader("shaders/bg.vs", "shaders/bg.fs");
 
     // allows key callback function to use the shaderProgram, as we are setting a pointer to it from window
-    glfwSetWindowUserPointer(window, &shaderProgram);
+    //glfwSetWindowUserPointer(window, &birdShader);
 
     // removes mouse cursor
-   // glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    //glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     stbi_set_flip_vertically_on_load(true);
 
     // texture setup
-    unsigned int birdTexture, bgTexture;
-    textureSetup(birdTexture, "images/flappy.png");
-    textureSetup(bgTexture, "images/bg.png");
+    unsigned int birdTexture = loadTexture("images/flappy.png");
+    unsigned int bgTexture = loadTexture("images/bg.png");
+
+    // set uniform values
+    birdShader.use();
+    glUniform1i(glGetUniformLocation(birdShader.ID, "birdTexture"), 0);
+
+    bgShader.use();
+    glUniform1i(glGetUniformLocation(bgShader.ID, "bgTexture"), 1);
 
     float birdVertices[] = {
         // positions          // texture coords
@@ -81,22 +98,55 @@ int main(){
         -0.06f, -0.10f, 0.0f,   0.0f, 0.0f,   // bottom left
         -0.06f,  0.10f, 0.0f,   0.0f, 1.0f    // top left 
     };
-    unsigned int birdIndices[] = { 
+    unsigned int quadIndices[] = { 
         0, 1, 3,
         1, 2, 3  
     };
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
+
+    float bgVertices[] = {
+        // positions          // texture coords
+         1.0f,  1.0f, 0.7f,   1.0f, 1.0f,   // top right
+         1.0f, -1.0f, 0.7f,   1.0f, 0.0f,   // bottom right
+        -1.0f, -1.0f, 0.7f,   0.0f, 0.0f,   // bottom left
+        -1.0f,  1.10f, 0.7f,   0.0f, 1.0f    // top left 
+    };
+
+    unsigned int VBO, birdVAO, EBO, bgVAO, bgVBO, bgEBO;
+
+    glGenVertexArrays(1, &birdVAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
 
-    glBindVertexArray(VAO);
+    glBindVertexArray(birdVAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(birdVertices), birdVertices, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(birdIndices), birdIndices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quadIndices), quadIndices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
+    glGenVertexArrays(1, &bgVAO);
+    glGenBuffers(1, &bgVBO);
+    glGenBuffers(1, &bgEBO);
+
+    glBindVertexArray(bgVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, bgVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(bgVertices), bgVertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, bgEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quadIndices), quadIndices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -112,45 +162,65 @@ int main(){
     // uncomment this call to draw in wireframe polygons.
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    // set uniform values
-    shaderProgram.use();
-    glUniform1i(glGetUniformLocation(shaderProgram.ID, "birdTexture"), 0);
+    glm::vec3 birdCurPos = glm::vec3(0.0f);
 
-    glm::mat4 birdMovement = glm::mat4(1.0f);
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window)){
         // render
         // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.2f, 0.3f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
-        
+
         if (flyUp == 0) {
-            birdMovement = glm::translate(birdMovement, glm::vec3(0.0f, -0.005, 0.0f));
+            birdCurPos.y = glm::max((float)(birdCurPos.y - 0.005), -1.0f);
         }
         else {
-            birdMovement = glm::translate(birdMovement, glm::vec3(0.0f, 0.01, 0.0f));
+            birdCurPos.y = glm::min((float)(birdCurPos.y + 0.01), 1.0f);
             flyUp--;
         }
-        glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "transform"), 1, GL_FALSE, glm::value_ptr(birdMovement));
 
+        glm::mat4 view = camera.GetViewMatrix();
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, birdCurPos);
+        birdShader.setMat4("model", model);
+        birdShader.setMat4("view", view);
+        birdShader.setMat4("projection", projection);
+
+        bgShader.use();
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, bgTexture);
+        glBindVertexArray(bgVAO);
+
+        view = camera.GetViewMatrix();
+        projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, -1.0f));
+
+        bgShader.setMat4("model", model);
+        bgShader.setMat4("view", view);
+        bgShader.setMat4("projection", projection);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
+        birdShader.use();
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, birdTexture);
-
-        shaderProgram.use();       
-        glBindVertexArray(VAO);
-
+        glBindVertexArray(birdVAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, &VAO);
+    glDeleteVertexArrays(1, &birdVAO);
+    glDeleteVertexArrays(1, &bgVAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
 
@@ -167,6 +237,33 @@ void processInput(GLFWwindow* window, int key, int scancode, int action, int mod
     else if (key == GLFW_KEY_SPACE && action != GLFW_RELEASE) {
         flyUp += 40;
     }
+    else if (key == GLFW_KEY_W && action != GLFW_RELEASE)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    else if (key == GLFW_KEY_S && action != GLFW_RELEASE)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    else if (key == GLFW_KEY_A && action != GLFW_RELEASE)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    else if (key == GLFW_KEY_D && action != GLFW_RELEASE)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+    if (firstMouseMovement) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouseMovement = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+    camera.ProcessMouseScroll(yoffset);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -175,23 +272,36 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height){
     glViewport(0, 0, width, height);
 }
 
-void textureSetup(unsigned int& id, const char* fp) {
-    glGenTextures(1, &id);
-    glBindTexture(GL_TEXTURE_2D, id);
+unsigned int loadTexture(char const* path) {
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    int width, height, nrChannels;
-    unsigned char* data = stbi_load(fp, &width, &height, &nrChannels, 0);
+    int width, height, nrComponents;
+    unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
     if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
     }
     else {
-        std::cout << "Failed to load texture" << std::endl;
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
     }
-    stbi_image_free(data);
+
+    return textureID;
 }
